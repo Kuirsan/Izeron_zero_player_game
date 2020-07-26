c7cc300f8df8de99e29a52c3eec94d2f15ec47cc
Kirill Pavlov <ksmersh@gmail.com>
2020-07-27 00:46:30 +0300

    Add initial inventory system

---
+++ Izeron.Library/Interfaces/ILootable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Izeron.Library.Interfaces
+{
+    public interface ILootable
+    {
+        void Loot();
+    }
+}
---
+++ Izeron.Library/InventorySystem/InventoryBase.cs
@@ -0,0 +1,15 @@
+﻿using Izeron.Library.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Izeron.Library.InventorySystem
+{
+    public abstract class InventoryBase
+    {
+        private protected int _cellNumber;
+        private protected List<ILootable> _inventory;
+        public abstract void Add(ILootable item);
+        public abstract void Remove(ILootable item);
+    }
+}
