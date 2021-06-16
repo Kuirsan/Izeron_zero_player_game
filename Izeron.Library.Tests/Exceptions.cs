d0ada87ca52423811abd1057dade18f9f96334d3
Kirill Pavlov <ksmersh@gmail.com>
2021-06-16 10:50:30 +0300

    add init test

---
+++ Izeron.Library.Tests/Exceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Izeron.Library.Tests
+{
+    public class Exceptions
+    {
+        [Fact]
+        public void testExceptions()
+        {
+            Assert.True(true);
+        }
+    }
+}
