32045271874f148ddbe2dbe5e513f33a92d9f0dc
Kirill Pavlov <ksmersh@gmail.com>
2021-08-12 00:30:06 +0300

    add quest guid system

--- QuesHandlerSystem.Library/Quest/Models/BaseQuestModel.cs
+++ QuesHandlerSystem.Library/Quest/Models/BaseQuestModel.cs
@@ -1,17 +1,21 @@
 ﻿using Izeron.Library.Persons;
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace QuestHandlerSystem.Library.Quest.Models
 {
     public abstract class BaseQuestModel
     {
+        private protected Guid _questGuid;
         public string Title { get; set; }
         public string Description { get; set; }
         public bool isProfessionQuest { get; set; } = false;
         public bool isBaseQuest { get; set; } = false;
-        public int? ParentID { get; set; }
+        public string ParentQuestGuid { get; set; }
+
+        public string QuestGuid => this.ToString();
 
         public bool isFinish = false;
 
@@ -19,11 +23,24 @@ namespace QuestHandlerSystem.Library.Quest.Models
         {
             Title = title;
             Description = description;
+            _questGuid = getGuid();
         }
 
         public abstract void getReward(AbstractPerson pers);
 
         public abstract string UpdateQuest();
-        
+
+        public override string ToString()
+        {
+            return _questGuid.ToString();
+        }
+
+        private Guid getGuid()
+        {
+            using MD5 md5 = MD5.Create();
+            byte[] hash = md5.ComputeHash(Encoding.Default.GetBytes(Title + Description));
+            return new Guid(hash);
+        }
+
     }
 }
